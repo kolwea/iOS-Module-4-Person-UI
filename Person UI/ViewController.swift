78e16bc7a00d3a65dda18c417c80560a00e24d01
Kolbe Weathington <kolbe.weathington@gmail.com>
2019-02-15 22:02:52 -0600

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Kolbe Weathington on 2/15/19.
+//  Copyright © 2019 Kolbe Weathington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
